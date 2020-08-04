f1cd76c4031cb08eaa8eb057b5fcf715f88e8de3
greg <gregorio.silva@localiza.com>
2020-08-04 08:47:06 -0300

    types.ts file created with exercises

--- app.ts
+++ app.ts
@@ -1,10 +1,1 @@
-/**
- * Crie uma classe Pickles de um tipo generics com os seguintes campos
- * add: (x: T, y: T) => T
- * getMaxValue: () => T
- * */
-
-
-/**
- * Crie um instância de Pickles e execute a função que retorna o valor máximo
- * */
+import './types';
---
+++ types.ts
@@ -0,0 +1,20 @@
+/**
+ * Crie um Enum Genius que guarda os seguintes valores 'opti-memory', 'gincobiloba' e 'lavitan'
+ */
+
+
+/**
+ * Crie uma função que retorna um valor do tipo Genius
+ */
+
+
+/**
+ * Crie uma classe Pickles de um tipo generics com os seguintes campos
+ * add: (x: T, y: T) => T
+ * getMaxValue: () => T
+ * */
+
+
+/**
+ * Crie um instância de Pickles e execute a função que retorna o valor máximo
+ * */
