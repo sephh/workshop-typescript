97b039771a94165cb4893c0aee150738dd015bfb
greg <gregorio.silva@localiza.com>
2020-08-04 13:12:41 -0300

    interface solution

--- interfaces.ts
+++ interfaces.ts
@@ -6,14 +6,49 @@
  * milk: number;
  * */
 
+interface CakeRecipe {
+    flour: number;
+    chocolate: number;
+    egg: number;
+    milk: number;
+}
+
 /**
  * Crie uma classe Cake que implementa CakeRecipe e tem um método retorna a caloria do bolo.
  * Pode inventar o cálculo das calorias com o valor dos ingredientes
  * getCalories: () => number
  * */
 
+class Cake implements CakeRecipe {
+    constructor(
+        public flour: number,
+        public chocolate: number,
+        public egg: number,
+        public milk: number
+    ) {
+    }
+
+    getCalories(): number {
+        return this.flour * this.chocolate * this.egg * this.milk;
+    }
+}
+
 /**
  * Crie a classe CakeFactory
  * recipe: CakeRecipe
  * bakeCake: () => Cake
  * */
+
+class CakeFactory {
+    constructor(public recipe: CakeRecipe) {
+    }
+
+    bakeCake(): Cake {
+        return new Cake(
+            this.recipe.flour,
+            this.recipe.chocolate,
+            this.recipe.egg,
+            this.recipe.milk,
+        );
+    }
+}
