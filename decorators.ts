fc9eeb3958784e52239b7dda10619fbfc9738e11
greg <gregorio.silva@localiza.com>
2020-08-05 15:48:33 -0300

    merge

--- decorators.ts
+++ decorators.ts
@@ -2,3 +2,26 @@
  * Crie o decorator ExecuteFourTimes
  * Quando o método for executado, execute ele 4 vezes
  * */
+
+const ExecuteFourTimes = (target, name: string, descriptor: PropertyDescriptor) => {
+    const method = descriptor.value;
+
+    descriptor.value = function (...args) {
+        const arr = [...new Array(4)];
+        arr.forEach((_, i) => {
+            method.apply(this, args);
+        });
+    }
+}
+
+
+class Example {
+    @ExecuteFourTimes
+    log(arg) {
+        console.log('Picles', arg);
+    }
+}
+
+const example = new Example();
+
+example.log('Gincobiloba');
