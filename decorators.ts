8d56d9d19c1b0086b8eb59402722534420cede7b
greg <gregorio.silva@localiza.com>
2020-08-06 04:07:33 -0300

    form validation with decorators

--- decorators.ts
+++ decorators.ts
@@ -24,4 +24,78 @@ class Example {
 
 const example = new Example();
 
-example.log('Gincobiloba');
+// example.log('Gincobiloba');
+
+/**
+ * Crie uma validação de atributos utilizando os decorators required e validate
+ * */
+
+const requiredFieldsKey = 'requiredFields';
+
+const defineRequiredFields = (target, propName: string) => {
+    if (Reflect.has(target, requiredFieldsKey)) {
+        const descriptor = Object.getOwnPropertyDescriptor(target, requiredFieldsKey);
+
+        descriptor.value[propName] = false
+    } else {
+        Object.defineProperty(target, requiredFieldsKey, {
+            value: {
+                [propName]: false
+            }
+        });
+    }
+}
+
+const required = (target, propName: string) => {
+    let value;
+
+    defineRequiredFields(target, propName);
+
+    Object.defineProperty(target, propName, {
+        get() {
+            return value;
+        },
+        set(v: any) {
+            value = v;
+            this[requiredFieldsKey][propName] = !!(value);
+        },
+    });
+}
+
+const validate = (target, propName: string, descriptor: PropertyDescriptor) => {
+    const method = descriptor.value;
+
+    descriptor.value = function (...args) {
+        const fieldsToValidate = this[requiredFieldsKey];
+
+        if (Object.values(fieldsToValidate).some(v => !v)) {
+            const invalidFields: string[] = [];
+
+            for (let key in fieldsToValidate) {
+                !fieldsToValidate[key] && invalidFields.push(key);
+            }
+
+            console.error(`Please, fill all required fields: ${invalidFields.join(', ')}`);
+        } else {
+            method.apply(this, args);
+        }
+    }
+}
+
+class UserForm {
+    @required
+    name: string;
+
+    @required
+    lastName: string;
+
+    @validate
+    submit() {
+        console.log('It\'s valid!!!', this.name, this.lastName);
+    }
+}
+
+const form = new UserForm();
+// form.name = 'Picles';
+// form.lastName = 'Silva';
+form.submit();
