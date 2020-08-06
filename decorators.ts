a3a5aff42355eff2df5fb48b8bb1fd9fc9038733
greg <gregorio.silva@localiza.com>
2020-08-06 04:08:13 -0300

    update decorators.ts

--- decorators.ts
+++ decorators.ts
@@ -2,3 +2,8 @@
  * Crie o decorator ExecuteFourTimes
  * Quando o método for executado, execute ele 4 vezes
  * */
+
+
+/**
+ * Crie uma validação de atributos utilizando os decorators required e validate
+ * */
